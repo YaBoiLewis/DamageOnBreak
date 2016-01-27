eb6987a69d9f78108dfa04ce09b6b3d992301bed
SavionLegendZzz <saviondeavault@gmail.com>
2016-01-27 17:13:29 -0500

    Create Main.php

---
+++ src/DOB/Main.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace DOB;
+
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\block\BlockBreakEvent;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utlis\TextFormat;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase implements Listener{
+	
+	public function onEnable(){
+		$this->getServer()->registerEvents($this,$this);
+		$this->getLogger()->info(TF::YELLOW."Loaded!");
+		@mkdir($this->getDataFolder());
+		$this->config = new Config($this->getDataFolder()."config.yml", Config::YAML, array("Damage" => 1.5));
+	}
+	
+	public fucntion onBreak(BlockBreakEvent $ev){
+		$player = $ev->getPlayer();
+		$cfg = $this->config->getAll();
+		if($ev->isCancelled()){
+			$player->setHeatlh($player->getHealth() - $cfg["Damage"]);
+			return;
+		}
+	}
+	
+	public function onDisable(){
+		$this->getLogger()->info(TF::RED."Disabling.....")
+	}
+}
